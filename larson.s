62a8c56df112cb5aa3bb4cffcc3410b4cbf86e68
theDarkKnight5050 <aiabdalla5050@gmail.com>
2020-01-22 12:26:10 -0800

    almost working

--- larson.s
+++ larson.s
@@ -9,41 +9,70 @@
  */
 
 .equ DELAY, 0x3F0000
+.equ NUMJMPS, 4 // the number of LEDS minus 1
 
-// configure GPIO 20 and 21 for output
+// configure GPIO 20-23 for output
 ldr r0, FSEL2
-mov r1, #1
+mov r1, #1 // output for 20
 orr r1, r1, #(1<<3)
 orr r1, r1, #(1<<6)
-orr r1, r1, #(1<<9)
-str r1, [r0]
+orr r1, r1, #(1<<9) // output for 23
+str r1, [r0] // set value in register
 
 // set bit 20 and 21
-mov r1, #(0b1010<<20)
+mov r1, #(0b0001<<20)
 
-loop: 
+cycle:
 
-// set GPIO 20 and 21 high
-ldr r0, SET0
-str r1, [r0] 
+	// decide led shift direction
+	cmp r1, #(0b0001<<20)
+	mov r3, #NUMJMPS
+	beq left
 
-// delay
-mov r2, #DELAY
-wait1:
-    subs r2, #1
-    bne wait1
+	right:
 
-// set GPIO 20 and 21 low
-ldr r0, CLR0
-str r1, [r0] 
+		// load new scanner pattern
+		lsr r1, r1, #1 
+		//mov r1, #(0001<<20)
+		ldr r0, SET0
+		str r1, [r0] 
 
-// delay
-mov r2, #DELAY
-wait2:
-    subs r2, #1
-    bne wait2
+		// delay
+		mov r2, #DELAY
+		wait1:
+			subs r2, #1
+			bne wait1
 
-b loop
+		// set GPIO 20-23 low
+		ldr r0, CLR0
+		str r1, [r0] 
+
+		subs r3, #1
+		bne right
+	b cycle
+
+	left:
+
+		// load new scanner pattern
+		lsl r1, r1, #1 
+		//mov r1, #(1000<<20)
+        ldr r0, SET0
+		str r1, [r0] 
+
+		// delay
+		mov r2, #DELAY
+		wait2:
+			subs r2, #1
+			bne wait2
+
+		// set GPIO 20-23 low
+		ldr r0, CLR0
+		str r1, [r0] 
+
+		subs r3, #1
+		bne left
+	b cycle
+
 
 FSEL0: .word 0x20200000
 FSEL1: .word 0x20200004
