d389c93bddd23e497e8917e94ff84d283ec5b963
Ahmed Abdalla <aabdalla@stanford.edu>
2020-02-18 14:19:22 -0800

    working shell

--- keyboard.c
+++ keyboard.c
@@ -1,8 +1,10 @@
 #include "gpio.h"
+#include "printf.h"
 #include "gpioextra.h"
 #include "keyboard.h"
 #include "ps2.h"
 
+static keyboard_modifiers_t mods;
 static unsigned int CLK, DATA;
 
 void keyboard_init(unsigned int clock_gpio, unsigned int data_gpio) 
@@ -17,29 +19,110 @@ void keyboard_init(unsigned int clock_gpio, unsigned int data_gpio)
 }
 
 
+static int read_bit(void) 
+{
+	while(!gpio_read(CLK)) {} // wait through low signal
+	while(gpio_read(CLK)) {} // wait for falling clock edge
+	return gpio_read(DATA);
+}
+
 unsigned char keyboard_read_scancode(void) 
 {
-    // TODO: Your code here
-    return 0x7e;
+    while(!read_bit()) {} // wait for start bit
+	unsigned char data = 0;
+	for(int i = 0; i < 8; i++)
+		data |= read_bit() << i;
+	read_bit();
+	read_bit();
+    return data;
 }
+
+// unsigned int read_scancode_helper(unsigned char *data){
+//     while(!read_bit()) {} // wait for start bit
+// 
+// 	unsigned int count = 0; // keeps track of parity
+// 	for(int i = 0; i < sizeof(*data); i++){
+// 		unsigned int next = read_bit(); 
+// 		if(next) count++; // increment parity
+// 	    data[0] |= next << i; // read (Little Endian) data
+// 	}
+// 
+// 	if(!((count + read_bit()) % 2)) return 0; // check parity
+// 	return read_bit(); // check stop bit
+// }
+// 
+// unsigned char keyboard_read_scancode(void) 
+// {
+// 	unsigned char data[1];
+// 	while(read_scancode_helper(data)){}
+//     return *data;
+// }
 
 key_action_t keyboard_read_sequence(void)
 {
-    // TODO: Your code here
     key_action_t action;
+    
+	unsigned char break_code = keyboard_read_scancode();
+	if(break_code != PS2_CODE_RELEASE){
+		action.what = KEY_PRESS;
+		action.keycode = break_code;
+	}
+	action.what = KEY_RELEASE;
+
+	unsigned char extend_code = keyboard_read_scancode();
+	if(extend_code != PS2_CODE_EXTENDED){
+		action.keycode = extend_code;
+	}
+
+	action.keycode = keyboard_read_scancode();
     return action;
 }
 
 key_event_t keyboard_read_event(void) 
 {
-    // TODO: Your code here
     key_event_t event;
+	event.action = keyboard_read_sequence();	
+	event.key = ps2_keys[event.action.keycode];
+
+	// Handle conditional modifiers
+	if(event.action.keycode == PS2_KEY_SHIFT){
+		if(event.action.what == KEY_PRESS) mods |= KEYBOARD_MOD_SHIFT; // turn bit on
+		else mods &= ~KEYBOARD_MOD_SHIFT; // turn bit off
+	}
+	if(event.action.keycode == PS2_KEY_ALT){
+		if(event.action.what == KEY_PRESS) mods |= KEYBOARD_MOD_ALT;
+		else mods &= ~KEYBOARD_MOD_ALT;
+	}
+	if(event.action.keycode == PS2_KEY_CTRL){
+		if(event.action.what == KEY_PRESS) mods |= KEYBOARD_MOD_CTRL;
+		else mods &= ~KEYBOARD_MOD_CTRL;
+	}
+
+	//Handle stick modifiers
+	if(event.action.keycode == PS2_KEY_CAPS_LOCK && event.action.what == KEY_PRESS){
+		mods ^= KEYBOARD_MOD_CAPS_LOCK; // toggle bit	
+	}	
+	if(event.action.keycode == PS2_KEY_SCROLL_LOCK && event.action.what == KEY_PRESS){
+		mods ^= KEYBOARD_MOD_SCROLL_LOCK;
+	}	
+	if(event.action.keycode == PS2_KEY_NUM_LOCK && event.action.what == KEY_PRESS){
+		mods ^= KEYBOARD_MOD_NUM_LOCK;
+	}	
+
+	event.modifiers = mods;	
     return event;
 }
 
 
+int is_letter(char ch){
+	return 'a' <= ch && ch <= 'z';
+}
+
 unsigned char keyboard_read_next(void) 
 {
-    // TODO: Your code here
-    return '!';
+	key_event_t event = keyboard_read_event();
+	if(event.key.other_ch == 0) return event.key.ch;
+    if(mods & KEYBOARD_MOD_SHIFT) return event.key.other_ch;
+	if(mods & KEYBOARD_MOD_CAPS_LOCK && is_letter(event.key.ch)) return event.key.other_ch;
+	return '!';
 }
--- shell.c
+++ shell.c
@@ -1,19 +1,61 @@
 #include "shell.h"
 #include "shell_commands.h"
 #include "uart.h"
+#include "keyboard.h"
+#include "malloc.h"
+#include "strings.h"
+#include "pi.h"
 
 #define LINE_LEN 80
 
 static formatted_fn_t shell_printf;
 
+/*
+ * Returns a pointer to a new null-terminated string containing at most `n`
+ * bytes copied from the string pointed to by `src`.
+ *
+ * Example: strndup("cs107e", 4) == "cs10"
+ */
+static char *strndup(const char *src, size_t n)
+{
+	char *dst = malloc(n + 1);
+	dst[0] = '\0';
+	strlcat(dst, src, n + 1);
+	return dst;
+}
+
+static bool isspace(char ch)
+{
+    return ch == ' ' || ch == '\t' || ch == '\n';
+}
+
+static int tokenize(const char *line, char *array[],  int max)
+{
+    int ntokens = 0;
+    const char *cur = line;
+
+    while (ntokens < max) {
+        while (isspace(*cur)) cur++;    // skip spaces (stop non-space/null)
+        if (*cur == '\0') break;        // no more non-space chars
+        const char *start = cur;
+        while (*cur != '\0' && !isspace(*cur)) cur++; // advance to end (stop space/null)
+        array[ntokens++] = strndup(start, cur - start);   // make heap-copy, add to array
+    }
+    return ntokens;
+}
+
+static const int NUM_COMMANDS = 5;
 static const command_t commands[] = {
     {"help",   "<cmd> prints a list of commands or description of cmd", cmd_help},
     {"echo",   "<...> echos the user input to the screen", cmd_echo},
+	{"reboot", "reboot the Raspberry Pi back to the bootloader using `pi_reboot", cmd_reboot},
+	{"peek", "<address> prints the contents (4 bytes) of memory at address", cmd_peek},
+	{"poke", "<address> <value> stores `value` into the memory at `address`", cmd_poke}
 };
 
 int cmd_echo(int argc, const char *argv[]) 
 {
-    for (int i = 1; i < argc; ++i) 
+    for (int i = 1; i < argc; i++) 
         shell_printf("%s ", argv[i]);
     shell_printf("\n");
     return 0;
@@ -21,8 +63,73 @@ int cmd_echo(int argc, const char *argv[])
 
 int cmd_help(int argc, const char *argv[]) 
 {
-    // TODO: your code here
-    return 0;
+    if(argc == 1){ // input: "help"
+		for(int i = 0; i < NUM_COMMANDS; i++){
+			shell_printf("%s: %s\n", commands[i].name, commands[i].description);
+		}
+		return 0;
+	} else{ // input: "help [cmd]"
+		for(int i = 0; i < NUM_COMMANDS; i++){
+			if(strcmp(commands[i].name, argv[1]) == 0){
+				shell_printf("%s: %s\n", commands[i].name, commands[i].description);
+				return 0;
+			}
+		}
+	}
+	shell_printf("error: no such command '%s'", argv[1]);
+    return -2;
+}
+
+int cmd_reboot(int argc, const char *argv[]){
+	pi_reboot();
+	return 0;
+}
+
+int to_num(const char *arg){
+	int addr = strtonum(arg, NULL);
+	if(addr == 0){
+		shell_printf("error: peek cannot convert '%s'\n", arg);
+		return -4;
+	}
+
+	if(addr % 4 != 0){
+		shell_printf("error: peek address must be 4-byte aligned\n");
+		return -5;
+	}
+
+	return addr;
+}
+
+int cmd_peek(int argc, const char *argv[]){
+	if(argc != 2){
+		shell_printf("error: peek expects 1 argument [address]\n");
+		return -3;
+	}
+	
+	int addr = to_num(argv[1]);
+	if(addr < 0) return addr;
+
+	shell_printf("%p: %08d\n", (void *)addr, *(unsigned int *)addr);
+	return 0;
+}
+
+int cmd_poke(int argc, const char *argv[]) {
+	if(argc != 3){
+		shell_printf("error: poke expects 2 argument [address] [value]\n");
+		return -6;
+	}
+	
+	int addr = to_num(argv[1]);
+	if(addr < 0) return addr;
+	
+	int val = strtonum(argv[2], NULL);
+	if(val == 0){
+		shell_printf("error: peek cannot convert '%s'\n", val);
+		return -4;
+	}
+
+	*(unsigned int *)addr = val;
+	return 0;
 }
 
 void shell_init(formatted_fn_t print_fn)
@@ -37,13 +144,48 @@ void shell_bell(void)
 
 void shell_readline(char buf[], size_t bufsize)
 {
-    // TODO: your code here
+	int line_size = 0;
+    while(1){
+		char next = keyboard_read_next();
+
+		if(next == '\n'){
+			shell_printf("\n");
+			buf[line_size] = '\0';
+			break;
+		} else if(next == '\b'){
+			if(line_size == 0) shell_bell();
+			else{
+				line_size--;
+
+				shell_printf("%c", '\b');
+				shell_printf(" ");
+				shell_printf("%c", '\b');
+			}	
+		} else if(line_size >= bufsize - 1){ // leaves space for null terminator
+			shell_bell();
+		} else{
+			shell_printf("%c", next);
+			buf[line_size] = next;
+			line_size++;
+		}
+	}
+	buf[line_size] = '\0';
 }
 
 int shell_evaluate(const char *line)
 {
-    // TODO: your code here
-    return 0;
+	int max = strlen(line);
+	char *arr[max];
+	int n_tokens = tokenize(line, arr, max); 
+	
+	for(int i = 0; i < NUM_COMMANDS; i++){
+		if(strcmp(commands[i].name, arr[0]) == 0){
+			return commands[i].fn(n_tokens, (const char **)arr);
+		}
+	}
+	
+	shell_printf("error: no such command %s\n", arr[0]);
+	return -1;
 }
 
 void shell_run(void)
